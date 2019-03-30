ac87f026ffe77730766fc328262638643a9632e6
Raghu Rudraraju <Raghu.Rudraraju@copart.com>
2019-03-30 23:20:28 +0530

    Converted graphData as dynamicData and converted home as component statefull.

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
 import {Tabs, Tab} from 'material-ui/Tabs';
@@ -9,6 +9,7 @@ import { reduceBy, forEachObjIndexed } from 'ramda';
 import classes from './Home.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import Aux from '../../hoc/Auxilary/Auxilary';
+import Piechart from '../UI/PieChart/pieChart';
 import Pie from '../UI/PieChart/pieChartNew';
 
 let width = 250;
@@ -24,12 +25,69 @@ const pieChartGraph = (data) => (
     </svg>
 )
 
-const home = (props) => {
+
+let formatGraphData = (data, valueField, labelField) => {
+  let formatGraphInnerData = reduceBy((acc, next) => acc + next[valueField], 0, (x) => x[labelField], data);
+
+  let finalArray = []
+
+  let convertAsArrayOfObj = (value, key) => {
+    finalArray.push({value: value, label: key});
+  }
+
+  forEachObjIndexed(convertAsArrayOfObj, formatGraphInnerData);
+  return finalArray
+}
+
+let userTransactionsGraphData = [];
+let userPaymentsGraphData = [];
+
+class home extends Component {
+  constructor(props) {
+    super();
+    this.state={
+      showMyTranscationsChart: false,
+      showMyPaymentsChart: false,
+    }
+  }
+
+  componentDidMount() {
+
+    if(this.props.userTransactions.length > 0) {
+      userTransactionsGraphData = formatGraphData(this.props.userTransactions, 'amount', 'paymentFrom');
+      this.setState({
+        showMyTranscationsChart: true
+      });
+    }
+    if(this.props.userPayments.length > 0){
+      userPaymentsGraphData = formatGraphData(this.props.userPayments, 'balance', 'description');
+      this.setState({
+        showMyPaymentsChart: true
+      });
+    }
+  }
+
+  componentWillReceiveProps(newProps) {
+    if(newProps.userTransactions.length > 0) {
+      userTransactionsGraphData = formatGraphData(newProps.userTransactions, 'amount', 'paymentFrom');
+      this.setState({
+        showMyTranscationsChart: true
+      });
+    }
+    if(newProps.userPayments.length > 0){
+      userPaymentsGraphData = formatGraphData(newProps.userPayments, 'balance', 'description');
+      this.setState({
+        showMyPaymentsChart: true
+      });
+    }
+  }
+
+  render() {
 
     return (
         <div style={{marginTop: '60px'}}>
             <Aux>
-                <Toolbar username={props.username}/>
+                <Toolbar username={this.props.username}/>
                 <div className={classes.UserTransactions}>
                     <div className={classes.Container}>
                         <Link to='/userTransactions' className={classes.Tile}>
@@ -61,7 +119,7 @@ const home = (props) => {
                               minHeight: '350px',
                             }}>
                             <h2 style={{marginTop:0}}>My Transactions</h2>
-                            {pieChartGraph([{label:"Credit", value:100},{label:"Debit", value:5},{label:"Transfer", value:3}])}
+                            {this.state.showMyTranscationsChart && pieChartGraph(userTransactionsGraphData)}
                           </div>
                         </Tab>
                         <Tab
@@ -75,7 +133,7 @@ const home = (props) => {
                               minHeight: '350px',
                             }}>
                             <h2 style={{marginTop:0}}>My Payments</h2>
-                            {pieChartGraph([{label:"Cash", value:500},{label:"Debit Card", value:2000},{label:"Others", value:1000}])}
+                            {this.state.showMyPaymentsChart && pieChartGraph(userPaymentsGraphData)}
                           </div>
                         </Tab>
                     </Tabs>
@@ -83,12 +141,16 @@ const home = (props) => {
             </div>
         </div>
     );
+  }
 }
 
 const mapStateToProps = (state) => {
   const { username } = state.login.userDetails;
+  const { userTransactions, userPayments} = state.home;
   return {
-      username
+      username,
+      userTransactions,
+      userPayments
   }
 }
 
